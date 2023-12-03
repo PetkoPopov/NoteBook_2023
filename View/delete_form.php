23fcabf9e7b0001aa2801686183daf3a65c6773c
pipjivko <pipjivko@gmail.com>
2023-12-04 00:14:00 +0200

    fix go back on delete button

--- View/delete_form.php
+++ View/delete_form.php
@@ -1,8 +1,16 @@
+<link rel="stylesheet" href="../Search/tableSearch.css"/>
+
 <?php
 $id = $_GET['id'];
 $newNameObject = $_GET['newNameObject'];
-var_dump($newNameObject,$id);
+//var_dump($newNameObject,$id);
 ?>
+<div style="text-align: center">
+    <form action="../funcShow.php" class="warning">
+    <input type="hidden" value="<?=$newNameObject?>" name="newNameObject"/>
+    <input type="submit" value="GO BACK" />
+</form>
+    
 <form  action="../proccess/delete.php">
     <input type="submit" value="are you sure to delete ??"><!-- comment -->
     <input type="hidden" value="<?=$id?>" name="id" />
@@ -10,3 +18,4 @@ var_dump($newNameObject,$id);
     
 </form>
     
+</div>
